bb42650144bbd1a0db5187c9b62b08dd4cafd201
sankodev <104733084+sankodev@users.noreply.github.com>
2022-05-03 17:49:01 -0700

    Add files via upload

---
+++ HourlyEmployeePay.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Project3_KSS_HourlyEmployeePay
+{
+    partial class HourlyEmployeePayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.payRateTextBox = new System.Windows.Forms.TextBox();
+            this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
+            this.overtimeHoursLabel = new System.Windows.Forms.Label();
+            this.overtimePayLabel = new System.Windows.Forms.Label();
+            this.totalPayLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kaleb Sandin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ITS 128";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Project3 Hourly Employee Pay";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Enter Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Enter Pay Rate:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Enter Hours Worked:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Overtime Hours:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 266);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Overtime Pay:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(40, 305);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Total Pay:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(284, 134);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // payRateTextBox
+            // 
+            this.payRateTextBox.Location = new System.Drawing.Point(284, 163);
+            this.payRateTextBox.Name = "payRateTextBox";
+            this.payRateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.payRateTextBox.TabIndex = 1;
+            // 
+            // hoursWorkedTextBox
+            // 
+            this.hoursWorkedTextBox.Location = new System.Drawing.Point(284, 191);
+            this.hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+            this.hoursWorkedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.hoursWorkedTextBox.TabIndex = 2;
+            // 
+            // overtimeHoursLabel
+            // 
+            this.overtimeHoursLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.overtimeHoursLabel.Location = new System.Drawing.Point(284, 231);
+            this.overtimeHoursLabel.Name = "overtimeHoursLabel";
+            this.overtimeHoursLabel.Size = new System.Drawing.Size(100, 23);
+            this.overtimeHoursLabel.TabIndex = 12;
+            // 
+            // overtimePayLabel
+            // 
+            this.overtimePayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.overtimePayLabel.Location = new System.Drawing.Point(284, 265);
+            this.overtimePayLabel.Name = "overtimePayLabel";
+            this.overtimePayLabel.Size = new System.Drawing.Size(100, 23);
+            this.overtimePayLabel.TabIndex = 13;
+            // 
+            // totalPayLabel
+            // 
+            this.totalPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalPayLabel.Location = new System.Drawing.Point(284, 304);
+            this.totalPayLabel.Name = "totalPayLabel";
+            this.totalPayLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalPayLabel.TabIndex = 14;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(68, 351);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "&Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(170, 351);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "C&lear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(271, 351);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // HourlyEmployeePayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 392);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.totalPayLabel);
+            this.Controls.Add(this.overtimePayLabel);
+            this.Controls.Add(this.overtimeHoursLabel);
+            this.Controls.Add(this.hoursWorkedTextBox);
+            this.Controls.Add(this.payRateTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "HourlyEmployeePayForm";
+            this.Text = "Hourly Employee Pay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox payRateTextBox;
+        private System.Windows.Forms.TextBox hoursWorkedTextBox;
+        private System.Windows.Forms.Label overtimeHoursLabel;
+        private System.Windows.Forms.Label overtimePayLabel;
+        private System.Windows.Forms.Label totalPayLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ HourlyEmployeePay.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project3_KSS_HourlyEmployeePay
+{
+    public partial class HourlyEmployeePayForm : Form
+    {
+        public HourlyEmployeePayForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Clear();
+            payRateTextBox.Clear();
+            hoursWorkedTextBox.Clear();
+            overtimeHoursLabel.Text = "";
+            overtimePayLabel.Text = "";
+            totalPayLabel.Text = "";
+            nameTextBox.Focus();
+            
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //prep
+            string name;
+            double payRate;
+            double hoursWorked;
+            double overtimeHours;
+            decimal overtimePay;
+            decimal totalPay;
+            
+            //reading is good :)
+            name = Convert.ToString(nameTextBox);
+            payRate = Convert.ToDouble(payRateTextBox.Text);
+            hoursWorked = Convert.ToDouble(hoursWorkedTextBox.Text);
+
+            payRate = Convert.ToDouble(payRateTextBox.Text);
+            //calc & if stat
+
+
+            if (hoursWorked <= 40)
+            {
+                totalPay = Convert.ToDecimal(hoursWorked * payRate);
+                totalPayLabel.Text = totalPay.ToString("C");
+                overtimeHoursLabel.Text = "None for you!";
+                overtimePayLabel.Text = "Trying to take extra money?";
+                overtimeHoursLabel.ForeColor = Color.IndianRed;
+                overtimePayLabel.ForeColor = Color.IndianRed;
+            }
+            else
+            {
+                //this is driving me craaaaaazy
+                overtimeHours = Convert.ToDouble(hoursWorked - 40);
+                overtimePay = Convert.ToDecimal(overtimeHours * payRate * 1.5);
+                totalPay = Convert.ToDecimal((overtimeHours * payRate * 1.5) + (payRate * 40));
+                totalPayLabel.Text = totalPay.ToString("C");
+                overtimePayLabel.ForeColor = Color.Green;
+                overtimePayLabel.Text = overtimePay.ToString("C");
+                overtimeHoursLabel.Text = Convert.ToString(overtimeHours);
+            }
+
+            hoursWorkedTextBox.Focus();
+            hoursWorkedTextBox.SelectAll();
+            //practice your converts!
+
+        }
+    }
+}
